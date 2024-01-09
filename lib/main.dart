5e867cdf10fcd170b5015c699d8ee56d3cb48e11
Anant Dubey <anantkumar312@gmail.com>
2024-01-09 15:13:35 +0530

    feat: :sparkles: add stateful widget class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz/start_screen.dart';
+import 'package:quiz/quiz.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,22 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
+    return const Quiz();
   }
 }
