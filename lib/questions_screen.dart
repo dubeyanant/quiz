7f78b307acb6d0264c15634e77282bcda98ee311
Anant Dubey <anantkumar312@gmail.com>
2024-01-10 15:31:15 +0530

    feat: :sparkles: add google font support

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
 import 'package:quiz/data/questions.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
@@ -35,7 +37,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
